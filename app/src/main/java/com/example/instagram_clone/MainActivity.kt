11e2f460d3d06d7865a0d1f39b201e5327d87bd5
sadulla <ubaydullayevsadulla843@gmail.com>
2021-11-16 11:12:29 +0500

    instagram clone

---
+++ app/src/main/java/com/example/instagram_clone/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.instagram_clone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mAuth = FirebaseAuth.getInstance()
+        val navController = findNavController(R.id.fragmentContainerView)
+        bottom_navigation.setupWithNavController(navController)
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (mAuth.currentUser == null){
+            startActivity(Intent(this,LoginActivity::class.java))
+            finish()
+        }
+    }
+}
